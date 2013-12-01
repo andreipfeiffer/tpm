5d15254a72fe67f46cf724cadf4c4af69c4a1aa2
Andrei Pfeiffer <andrei@upsidedown.ro>
2013-12-01 23:36:06 +0200

    Update db

--- modules/db.js
+++ modules/db.js
@@ -3,7 +3,7 @@ var config = require('../config');
 module.exports = function(connection) {
 
 	// Create database, if it doesn't exist
-	connection.query('CREATE DATABASE IF NOT EXISTS ' + config.mysql.database, function (err) {
+	connection.query('CREATE DATABASE IF NOT EXISTS ' + config.mysql.database + ' DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci', function (err) {
 		if (err) {
 			throw err;
 		}
@@ -12,25 +12,29 @@ module.exports = function(connection) {
 			if (err) {
 				throw err;
 			}
-			// Update the structure
-			connection.query(config.mysql.structure, function (err) {
-				if (err) {
-					throw err;
-				}
-				// @todo temporary: insert some fixtures, if database is empty
-				connection.query('select * from `todos`', function (err, docs) {
+
+			Object.keys(config.mysql.structure).forEach(function(item){
+				// Update the structure
+				connection.query(config.mysql.structure[item], function (err) {
 					if (err) {
 						throw err;
 					}
-					if ( docs.length === 0 ) {
-						connection.query(config.mysql.populate, function (err) {
-							if (err) {
-								throw err;
-							}
-						});
-					}
+					// @todo temporary: insert some fixtures, if database is empty
+					connection.query('select * from `' + item + '`', function (err, docs) {
+						if (err) {
+							throw err;
+						}
+						if ( docs.length === 0 ) {
+							connection.query(config.mysql.populate[item], function (err) {
+								if (err) {
+									throw err;
+								}
+							});
+						}
+					});
 				});
 			});
+
 		});
 	});
 
