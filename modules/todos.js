405c960eb5bb9f7357610c80ff3f29907fe0263c
Andrei Pfeiffer <andrei@upsidedown.ro>
2013-12-05 01:34:03 +0200

    Fix ID update when adding, from database to Ember's store

--- modules/todos.js
+++ modules/todos.js
@@ -2,6 +2,12 @@ module.exports = function(connection) {
 
     'use strict';
 
+    var getTodoById = function(id, idUser, callback) {
+        connection.query('select * from `todos` where `id`="' + id + '" AND `idUser`="' + idUser + '"', function(err, docs) {
+            callback(err, docs);
+        });
+    };
+
     return {
         getAll: function(req, res) {
             var userLogged = req.user;
@@ -12,33 +18,17 @@ module.exports = function(connection) {
             });
         },
 
-        /*getById: function(req, res) {
+        getById: function(req, res) {
             var id = req.params.id,
                 userLogged = req.user;
 
-            if( isNaN(parseInt( req.params.id, 10 )) ) {
-                return res.send('TypeError: a number is required');
-            }
+            getTodoById(id, userLogged.id, function(err, docs) {
+                if (err) { return res.send(503, { error: 'Database error'}); }
+                if (!docs) { return res.send(410, { error: 'Record not found'}); }
 
-            console.log('Retrieving todo: ' + id);
-            db.collection('todos', function(err, collection) {
-                collection.findOne({'id': parseInt(id, 10)}, function(err, item) {
-
-                    if (err) {
-                        res.statusCode = 404;
-                        return res.send('Error: something went wrong');
-                    }
-
-                    if (!item) {
-                        res.statusCode = 404;
-                        return res.send('Error 404: todo with id="' + id + '" was not found');
-                    }
-
-                    res.send(item);
-                });
+                res.send({'todo': docs[0]});
             });
-
-        },*/
+        },
 
         update: function(req, res) {
             var id = parseInt( req.params.id, 10 ),
@@ -56,10 +46,17 @@ module.exports = function(connection) {
             var data = req.body.todo,
                 userLogged = req.user;
 
-            connection.query('insert into `todos` (`idUser`, `title`, `isCompleted`) values ("' + userLogged.id + '", "' + data.title + '", "' + data.isCompleted + '")', function(err) {
+            connection.query('insert into `todos` (`idUser`, `title`, `isCompleted`) values ("' + userLogged.id + '", "' + data.title + '", "' + data.isCompleted + '")', function(err, newItem) {
                 if (err) { return res.send(503, { error: 'Database error'}); }
 
-                res.send(true);
+                getTodoById(newItem.insertId, userLogged.id, function(err, docs) {
+                    if (err) { return res.send(503, { error: 'Database error'}); }
+                    if (!docs) { return res.send(410, { error: 'Record not found'}); }
+
+                    // need to return an object that contains all the data including the database id
+                    // so Ember can update its store, with the new ID from database
+                    res.send({'todo': docs[0]});
+                });
             });
 
         },
@@ -72,8 +69,7 @@ module.exports = function(connection) {
                 if (err) { return res.send(503, { error: 'Database error'}); }
 
                 if ( docs.length === 0 ) {
-                    res.statusCode = 404;
-                    return res.send('id "' + id + '" was not found');
+                    return res.send(404, { error: 'id "' + id + '" was not found'});
                 } else {
                     connection.query('delete from `todos` where `idUser`="' + userLogged.id + '" AND `id`="' + id + '"', function(err) {
                         if (err) { return res.send(503, { error: 'Database error'}); }
--- public/js/models/todos.js
+++ public/js/models/todos.js
@@ -3,6 +3,7 @@
     'use strict';
 
     Todos.Todo = DS.Model.extend({
+        // id: DS.attr('number'),
         idUser: DS.attr('number'),
         title: DS.attr('string'),
         isCompleted: DS.attr('boolean')
