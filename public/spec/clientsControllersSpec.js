beff340382ff98c77f6b36128cd7c0bee9f81871
Andrei Pfeiffer <andrei@upsidedown.ro>
2014-07-27 17:51:00 +0300

    Unit-tests: Clients Add

--- public/spec/clientsControllersSpec.js
+++ public/spec/clientsControllersSpec.js
@@ -25,6 +25,31 @@
                 expect(scope.clientsList).toEqualDeep( TPM.mocks.clientsList );
             });
 
+            it('should toggle the New Client form', function() {
+                $httpBackend.flush();
+
+                expect(scope.isFormNewDisplayed).toBeFalsy();
+                scope.toggleNewFormDisplay();
+                expect(scope.isFormNewDisplayed).toBeTruthy();
+            });
+
+            it('should add a new client', function() {
+                var nr = TPM.mocks.clientsList.length,
+                    name = 'New client';
+
+                $httpBackend.flush();
+
+                scope.toggleNewFormDisplay();
+                scope.newClient.name = name;
+
+                $httpBackend.expectPOST(TPM.apiUrl + 'clients').respond(201);
+                scope.addNewClient();
+                $httpBackend.flush();
+
+                expect(scope.clientsList.length).toBe( nr + 1 );
+                expect(scope.newClient.name).toBe('');
+            });
+
         });
     });
 
