895cafbdae6eb307c960baafbabb4d1657504f71
Andrei Pfeiffer <andrei@upsidedown.ro>
2014-07-30 15:51:44 +0300

    Update production config

--- server/config.js
+++ server/config.js
@@ -11,7 +11,7 @@ module.exports = (function(){
                     host: 'localhost',
                     user: 'root',
                     password: '',
-                    database: 'tpm'
+                    database: 'upsidedown_ro_tpm'
                 }
             };
 
