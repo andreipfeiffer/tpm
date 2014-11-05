b5cd03511fc6488b6ebc36e37aa2d1d3cf515c61
andrei@upsidedown.ro <andrei@upsidedown.ro>
2014-11-05 18:16:50 +0200

    Unit-tests: MenuController

---
+++ public/spec/headerControllersSpec.js
@@ -0,0 +1,32 @@
+(function() {
+
+    'use strict';
+
+    describe('Header Controllers', function() {
+
+        beforeEach(module('tpm'));
+
+        describe('MenuController', function() {
+            var scope, ctrl, location;
+
+            beforeEach(inject(function($rootScope, $controller, $location) {
+                location = $location;
+                scope = $rootScope.$new();
+                ctrl = $controller('MenuController', {$scope: scope});
+            }));
+
+
+            it('should return true, if the location path matches', function() {
+                location.path('/projects');
+                expect(scope.isActive('projects')).toBeTruthy();
+            });
+
+            it('should return false, if the location path does not match', function() {
+                location.path('/asdasd');
+                expect(scope.isActive('projects')).toBeFalsy();
+            });
+
+        });
+    });
+
+})();
