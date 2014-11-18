ae9cead00c73361a961d5caba4431b13844c1e78
Andrei Pfeiffer <andrei@upsidedown.ro>
2014-11-18 22:02:54 +0200

    Settings: add notification after access revoke

--- public/js/controllers/settingsControllers.js
+++ public/js/controllers/settingsControllers.js
@@ -7,9 +7,9 @@
         .controller('SettingsController', [
             '$scope',
             '$http',
-            // 'ngNotify',
+            'ngNotify',
             'SettingsService',
-            function($scope, $http, /*ngNotify, */Settings) {
+            function($scope, $http, ngNotify, Settings) {
 
                 $scope.isLoading = true;
                 $scope.isLoadingGoogle = false;
@@ -38,6 +38,7 @@
                     $http.delete(TPM.apiUrl + 'auth/google').success(function () {
                         $scope.settings.googleToken = false;
                         $scope.isLoadingGoogle = false;
+                        ngNotify.set('Your Google Calendar is not syncronized anymore');
                     });
                 };
 
