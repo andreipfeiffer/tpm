cd49ad53eae9a8f8b5956a853f8673ebe1ae6ab2
Andrei Pfeiffer <andrei@upsidedown.ro>
2014-11-28 23:39:22 +0200

    Update feedback dependency path, from bower repo

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function(config) {
             '../bower_components/angular-ui-utils/ui-utils.min.js',
             '../bower_components/angular-touch/angular-touch.min.js',
             '../bower_components/angular-media-queries/match-media.js',
-            '../bower_components/feedback/src/scripts/feedback.js',
+            '../bower_components/angular-feedback/dist/feedback.min.js',
 
             // vendor helpers
             '../bower_components/angular-mocks/angular-mocks.js',
