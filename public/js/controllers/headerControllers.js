bc4feeb0357a650e46aaf0fe095461d3e3e35992
Andrei Pfeiffer <andrei@upsidedown.ro>
2014-12-07 12:45:47 +0200

    Restrict menu collapse trigger only on small screens

--- public/js/controllers/headerControllers.js
+++ public/js/controllers/headerControllers.js
@@ -7,15 +7,15 @@
         .controller('MenuController', [
             '$scope',
             '$location',
-            function($scope, $location) {
+            'screenSize',
+            function($scope, $location, screenSize) {
 
                 $scope.isActive = function (path) {
                     return ( $location.path().indexOf( path ) > -1 );
                 };
 
                 $scope.collapseMenu = function() {
-                    console.log('clicked');
-                    angular.element('#navbar-toggle').trigger('click');
+                    screenSize.is('xs') && angular.element('#navbar-toggle').trigger('click');
                 };
 
             }
