d9383cb61d944a7488df163c749814ba944fe72e
Andrei Pfeiffer <andrei@upsidedown.ro>
2014-12-07 19:24:18 +0200

    Add build task to Grunt

--- Gruntfile.js
+++ Gruntfile.js
@@ -4,11 +4,15 @@ module.exports = function(grunt) {
 
     'use strict';
 
+    var packageData = grunt.file.readJSON('package.json');
+
     require('matchdep').filterDev('grunt-*').forEach(grunt.loadNpmTasks);
+
+    grunt.loadTasks('tasks');
 
     grunt.initConfig({
 
-        pkg: grunt.file.readJSON('package.json'), // the package file to use
+        pkg: packageData, // the package file to use
 
         karma: {
             unit:  { configFile: 'karma.conf.js', keepalive: true },
@@ -39,20 +43,6 @@ module.exports = function(grunt) {
             }
         },
 
-        uglify: {
-            options: {
-                // the banner is inserted at the top of the output
-                banner: '/*\n<%= pkg.name %> v<%= pkg.version %> \n \nLast update: <%= grunt.template.date() %> \nAuthor: <%= pkg.author.name %> (<%= pkg.author.url %>)\n<%= pkg.repository.url %>\n*/\n',
-                sourceMap: 'src/source-map.js',
-                report: 'gzip'
-            },
-            dist: {
-                files: {
-                    'src/<%= pkg.name %>-<%= pkg.version %>.min.js': ['src/jquery.esoh-lift.js']
-                }
-            }
-        },
-
         /*mochacli: {
             options: {
                 reporter: 'spec',
@@ -71,6 +61,113 @@ module.exports = function(grunt) {
             }
         },
 
+        jade: {
+            compile: {
+                options: {
+                    pretty: true,
+                    data: packageData
+                },
+                files: {
+                    'temp/index.html': ['public/index.jade']
+                }
+            }
+        },
+
+        dom_munger: {
+            build: {
+                options: {
+                    read: [
+                        {
+                            selector: 'script',
+                            attribute: 'src',
+                            writeto: 'jsRefs',
+                            isPath: false
+                        },
+                        {
+                            selector: 'link',
+                            attribute: 'href',
+                            writeto: 'cssRefs',
+                            isPath: false
+                        }
+                    ],
+                    remove: ['link','script'],
+                    append: [
+                        { selector:'head', html:'<link href="dist/css/<%= pkg.name %>-<%= pkg.version %>.css" rel="stylesheet">' },
+                        { selector: 'body', html: '<script src="dist/js/<%= pkg.name %>-<%= pkg.version %>.js"></script>' }
+                    ]
+                },
+                // overwrite file
+                src: 'temp/index.html'
+            },
+        },
+
+        concat: {
+            css: {
+                src: '<%= resources.css %>',
+                dest:'temp/style.css'
+            },
+            js: {
+                src: '<%= resources.js %>',
+                dest:'temp/script.js'
+            }
+        },
+
+        cssmin: {
+            build: {
+                options: {
+                    banner: '/*\n<%= pkg.name %> v<%= pkg.version %> \n \nLast update: <%= grunt.template.date() %> \nAuthor: <%= pkg.author.name %> (<%= pkg.author.url %>)\n<%= pkg.repository.url %>\n*/\n',
+                    report: 'gzip',
+                    keepSpecialComments: 0
+                },
+                files: {
+                    'dist/css/<%= pkg.name %>-<%= pkg.version %>.css': ['temp/style.css']
+                }
+            }
+        },
+
+        htmlmin: {
+            build: {
+                options: {
+                    removeComments: true,
+                    collapseWhitespace: true,
+                    // conservativeCollapse: true,
+                    preserveLineBreaks: true
+                },
+                files: {
+                    'dist/index.html': 'temp/index.html'
+                }
+            }
+        },
+
+        uglify: {
+            build: {
+                options: {
+                    banner: '/*\n<%= pkg.name %> v<%= pkg.version %> \n \nLast update: <%= grunt.template.date() %> \nAuthor: <%= pkg.author.name %> (<%= pkg.author.url %>)\n<%= pkg.repository.url %>\n*/\n',
+                    report: 'gzip',
+                    mangle: false
+                },
+                files: {
+                    'dist/js/<%= pkg.name %>-<%= pkg.version %>.js': ['temp/script.js']
+                }
+            }
+        },
+
+        copy: {
+            fonts: {
+                expand: true,
+                cwd: 'bower_components/bootstrap/dist/fonts/',
+                src: '**',
+                dest: 'dist/fonts/',
+                flatten: true,
+                filter: 'isFile'
+            }
+        },
+
+        clean: {
+            dist: ['dist'],
+            temp: ['temp']
+        },
+
         // watch: {
         //     files: ['src/*.js', 'spec/*.js', 'css/*.css'],
         //     tasks: ['jshint', 'csslint'],
@@ -79,6 +176,20 @@ module.exports = function(grunt) {
 
     grunt.registerTask('default', [
         'jshint', 'mocha_istanbul', 'karma'
+    ]);
+
+    grunt.registerTask('build', [
+        'default',
+        'clean:dist',
+        'jade',
+        'dom_munger',
+        'fix_public_path',
+        'concat',
+        'htmlmin',
+        'cssmin',
+        'uglify',
+        'copy',
+        'clean:temp'
     ]);
 
     // run this first:
