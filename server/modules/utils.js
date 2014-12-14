3278190a25615e1bf4ac1c203d83b13284fbdf53
Andrei Pfeiffer <andrei@upsidedown.ro>
2014-12-15 00:17:34 +0200

    Fix error log insertion in database

--- server/modules/utils.js
+++ server/modules/utils.js
@@ -3,19 +3,20 @@ module.exports = (function() {
     'use strict';
 
     var server = require('../../server'),
+        app = server.app,
         knex = server.knex;
 
-    server.app.on('logError', function(o) {
+    app.on('logError', function(o) {
 
-        console.error('[tpm_error]: ' + new Date().toString());
-        console.trace(o);
-
-        return knex('error_log')
+        knex('error_log')
             .insert({
                 idUser: o.idUser,
                 source: o.source,
                 data: o.data || '',
                 error: JSON.stringify( o.error )
+            }).then(function() {
+                console.error('[tpm_error]: ' + new Date().toString());
+                console.trace(o);
             });
     });
 })();
