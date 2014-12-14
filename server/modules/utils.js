19c945c2bef25f97c4d1c25e4fa0b796c4c3bf03
Andrei Pfeiffer <andrei@upsidedown.ro>
2014-12-15 00:20:33 +0200

    Utils: add catch statement

--- server/modules/utils.js
+++ server/modules/utils.js
@@ -7,16 +7,16 @@ module.exports = (function() {
         knex = server.knex;
 
     app.on('logError', function(o) {
-
-        knex('error_log')
-            .insert({
-                idUser: o.idUser,
-                source: o.source,
-                data: o.data || '',
-                error: JSON.stringify( o.error )
-            }).then(function() {
-                console.error('[tpm_error]: ' + new Date().toString());
-                console.trace(o);
-            });
+        knex('error_log').insert({
+            idUser: o.idUser,
+            source: o.source,
+            data: o.data || '',
+            error: JSON.stringify( o.error )
+        }).then(function() {
+            console.error('[tpm_error]: ' + new Date().toString());
+            console.trace(o);
+        }).catch(function(err) {
+            console.error(err);
+        });
     });
 })();
