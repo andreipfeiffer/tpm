98af18a501b5f99da9df35c3e2ee3a5600fcd97f
Andrei Pfeiffer <andrei@upsidedown.ro>
2015-11-06 11:51:17 +0200

    Make code more readable

--- server/modules/reports.js
+++ server/modules/reports.js
@@ -5,13 +5,26 @@ module.exports = function(knex) {
     function getProjectsReport(idUser) {
         var q = '';
 
-        q += 'SELECT projects.id, idClient, projects.name, clients.name AS clientName, priceEstimated, priceFinal, date, projects.status, CONCAT(YEAR(filtered_projects.date), "-" , MONTH(filtered_projects.date)) AS month';
+        q += 'SELECT';
+        q += ' projects.id,';
+        q += ' idClient,';
+        q += ' projects.name,';
+        q += ' clients.name AS clientName,';
+        q += ' priceEstimated,';
+        q += ' priceFinal,';
+        q += ' date,';
+        q += ' projects.status,';
+        q += ' CONCAT(YEAR(filtered_projects.date), "-" , MONTH(filtered_projects.date)) AS month';
+
         q += ' FROM (SELECT date, idProject FROM projects_status_log WHERE idUser='+idUser+' AND status IN ("paid", "finished") ORDER BY date DESC)';
         q += ' AS filtered_projects';
+
         q += ' LEFT JOIN projects';
         q += ' ON filtered_projects.idProject = projects.id';
+
         q += ' LEFT JOIN clients';
         q += ' ON projects.idClient = clients.id';
+
         q += ' WHERE projects.isDeleted = 0';
         q += ' GROUP BY filtered_projects.idProject';
         q += ' ORDER BY filtered_projects.date DESC';
