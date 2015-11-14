7a5d619f5f2a0114653d3419137556e0e8b6dbfc
Andrei Pfeiffer <andrei@upsidedown.ro>
2015-11-14 13:27:52 +0200

    Fix alignment

--- public/spec/_mocks.js
+++ public/spec/_mocks.js
@@ -6,51 +6,51 @@ var TPM = TPM || {};
 
         projectsList: [
             {
-                "id": 1,
-                "idUser": 1,
-                "idClient": 1,
-                "name": "Pufosenie roz",
-                "status": "on hold",
-                "price": 0,
-                "dateAdded": "2014-07-22T21:00:00.000Z",
+                "id"           : 1,
+                "idUser"       : 1,
+                "idClient"     : 1,
+                "name"         : "Pufosenie roz",
+                "status"       : "on hold",
+                "price"        : 0,
+                "dateAdded"    : "2014-07-22T21:00:00.000Z",
                 "dateEstimated": "2014-12-29T22:00:00.000Z"
             },
             {
-                "id": 2,
-                "idUser": 1,
-                "idClient": 0,
-                "name": "Album foto",
-                "status": "in progress",
-                "price": 20,
-                "dateAdded": "2014-07-22T21:00:00.000Z",
+                "id"           : 2,
+                "idUser"       : 1,
+                "idClient"     : 0,
+                "name"         : "Album foto",
+                "status"       : "in progress",
+                "price"        : 20,
+                "dateAdded"    : "2014-07-22T21:00:00.000Z",
                 "dateEstimated": "2014-09-11T21:00:00.000Z"
             },
             {
-                "id": 3,
-                "idUser": 1,
-                "idClient": 2,
-                "name": "Bratari duble",
-                "status": "in progress",
-                "price": 100,
-                "dateAdded": "2014-07-22T21:00:00.000Z",
+                "id"           : 3,
+                "idUser"       : 1,
+                "idClient"     : 2,
+                "name"         : "Bratari duble",
+                "status"       : "in progress",
+                "price"        : 100,
+                "dateAdded"    : "2014-07-22T21:00:00.000Z",
                 "dateEstimated": "2014-09-11T21:00:00.000Z"
             }
         ],
 
         clientsList: [
             {
-                "id": 1,
-                "idUser": 1,
-                "name": "client Ana",
+                "id"         : 1,
+                "idUser"     : 1,
+                "name"       : "client Ana",
                 "dateCreated": "2014-07-14T19:10:55.000Z",
-                "nrProjects": 2
+                "nrProjects" : 2
             },
             {
-                "id": 2,
-                "idUser": 1,
-                "name": "client Ion",
+                "id"         : 2,
+                "idUser"     : 1,
+                "name"       : "client Ion",
                 "dateCreated": "2014-07-14T19:10:55.000Z",
-                "nrProjects": 1
+                "nrProjects" : 1
             }
         ]
 
