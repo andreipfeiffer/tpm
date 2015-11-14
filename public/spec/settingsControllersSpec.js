58ee48df9ffa5f38abcea477bc89dda383c3ff3e
Andrei Pfeiffer <andrei@upsidedown.ro>
2015-11-14 15:10:51 +0200

    Tests for SettingsUser

--- public/spec/settingsControllersSpec.js
+++ public/spec/settingsControllersSpec.js
@@ -75,6 +75,36 @@
             });
 
         });
+
+
+        describe('SettingsUser', function() {
+            var scope, ctrl, $httpBackend, SettingsUser;
+
+            beforeEach(inject(function(_$httpBackend_, $rootScope, $controller, _SettingsUser_) {
+                SettingsUser = _SettingsUser_;
+                $httpBackend = _$httpBackend_;
+                $httpBackend.expectGET(TPM.apiUrl + 'settings/google').respond({
+                    googleToken: false
+                });
+
+                scope = $rootScope.$new();
+                ctrl  = $controller('SettingsController', {$scope: scope});
+            }));
+
+            it('should saveUserSettings()', function() {
+                $httpBackend.flush();
+                expect( scope.user.data.currency ).toEqual('RON');
+
+                scope.user.data.currency = 'EUR';
+
+                $httpBackend.expectPUT(TPM.apiUrl + 'settings/user', scope.user.data).respond( 200 );
+                scope.saveUserSettings();
+                $httpBackend.flush();
+
+                expect( SettingsUser.get().currency ).toEqual('EUR');
+            });
+
+        });
     });
 
 })();
