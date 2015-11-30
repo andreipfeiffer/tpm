e4fa2dcee6477bc21b5334817f69fee6b5932e94
Andrei Pfeiffer <andrei@upsidedown.ro>
2015-11-30 01:20:22 +0100

    Refactor services: extract class SettingsUser

--- public/js/services.js
+++ public/js/services.js
@@ -1,37 +1,7 @@
 import angular from 'angular';
-import utils from 'public/js/utils';
 import config from 'public/js/appConfig';
 import SessionService from 'public/js/components/SessionService';
-
-// @todo extract module
-class SettingsUser {
-    constructor($http) {
-        this.$http    = $http;
-        // @todo use Map()
-        this.defaults = {
-            currency: utils.currencyList[0]
-        };
-    }
-
-    fetch() {
-        return this.$http.get(config.getApiUrl() + 'settings/user');
-    }
-    remove() {
-        return localStorage.removeItem('TPMsettings');
-    }
-    set(data) {
-        var _settings = Object.assign( {}, this.defaults, data );
-        localStorage.setItem('TPMsettings', JSON.stringify( _settings ));
-    }
-    get() {
-        var settings = JSON.parse( localStorage.getItem('TPMsettings') );
-        if ( !settings ) {
-            settings = Object.assign( {}, this.defaults );
-        }
-        return settings;
-    }
-}
-SettingsUser.$inject = ['$http'];
+import SettingsUser from 'public/js/components/SettingsUser';
 
 export default angular.module('TPM.Services', ['ngResource'])
 
