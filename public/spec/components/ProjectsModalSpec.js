9e92dc1a8e4acce4632ee2dae0a34d365d403815
Andrei Pfeiffer <andrei@upsidedown.ro>
2015-12-01 13:09:34 +0100

    Tests for ProjectsModal

---
+++ public/spec/components/ProjectsModalSpec.js
@@ -0,0 +1,31 @@
+import 'angular';
+import 'angular-animate';
+import 'angular-mocks';
+import 'public/js/app';
+import stubs from 'public/spec/_stubs';
+
+describe('ProjectsModal', () => {
+
+    const MODAL_TITLE = 'projects modal title';
+    var ProjectsModal, $modal, $httpBackend, scope;
+
+    beforeEach(angular.mock.module('tpm'));
+
+    beforeEach(inject((_$httpBackend_, _ProjectsModal_, $uibModal) => {
+        ProjectsModal = _ProjectsModal_;
+        $httpBackend  = _$httpBackend_;
+        $modal        = $uibModal;
+
+        // stub the template
+        $httpBackend.whenGET(/views\//).respond('<h3 class="modal-title">{{title}}</h3>');
+    }));
+
+
+    it('should open ProjectsModal', () => {
+        ProjectsModal.open(MODAL_TITLE, [], false);
+        $httpBackend.flush();
+
+        expect( $('.modal-title').text() ).toBe( MODAL_TITLE );
+    });
+
+});
