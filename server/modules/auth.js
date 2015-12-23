3d780be5d282d9ec4726d3d266b6567925b9089d
Andrei Pfeiffer <andrei@upsidedown.ro>
2015-12-23 02:08:28 +0200

    Uncomment generateSalt()

--- server/modules/auth.js
+++ server/modules/auth.js
@@ -15,15 +15,15 @@ module.exports = (() => {
         moment        = require('moment');
 
     // private encryption & validation methods
-    // function generateSalt() {
-    //     var set = '0123456789abcdefghijklmnopqurstuvwxyzABCDEFGHIJKLMNOPQURSTUVWXYZ';
-    //     var salt = '';
-    //     for (var i = 0; i < 10; i += 1) {
-    //         var p = Math.floor(Math.random() * set.length);
-    //         salt += set[p];
-    //     }
-    //     return salt;
-    // };
+    function generateSalt() {
+        var set = '0123456789abcdefghijklmnopqurstuvwxyzABCDEFGHIJKLMNOPQURSTUVWXYZ';
+        var salt = '';
+        for (var i = 0; i < 10; i += 1) {
+            var p = Math.floor(Math.random() * set.length);
+            salt += set[p];
+        }
+        return salt;
+    };
 
     function md5(str) {
         return crypto.createHash('md5').update(str).digest('hex');
